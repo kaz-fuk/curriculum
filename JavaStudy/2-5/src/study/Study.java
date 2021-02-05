0bc539a204be5badf57425662c08ec2eea2347ea
kaz-fuk <kazfuk1209@gmail.com>
2021-02-05 22:39:23 +0900

    課題提出（再）
    リスト、ハッシュ

--- JavaStudy/2-5/src/study/Study.java
+++ JavaStudy/2-5/src/study/Study.java
@@ -40,7 +40,7 @@ public class Study {
     // ③ カリキュラムを参考に拡張for文を使って、課題の画像と同じ表示になるよう記述してください。
     // 「shohinList」と「shopMap」が保持する値を上手く利用しましょう。
     for (int i = 0; i < shopMap.size(); i++) {
-      System.out.println(shohinList.get(i) + "=" + shopMap.get(shohinList.get(i)) + "になります！");
+      System.out.println(shohinList.get(i) + "=" + shopMap.get(shohinList.get(i)) + "円になります！");
     }
 
   }
