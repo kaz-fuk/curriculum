a5b4ab66303b42ceca18730bc22ef5fd09010eda
kaz-fuk <kazfuk1209@gmail.com>
2021-02-09 22:33:43 +0900

    チェックテスト
    微調整

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -4,18 +4,18 @@ import constants.Constants;
 
 public class Check {
 
-  private static String firstName = "福地";
-  private static String lastName = "和樹";
+	private static String lastName = "福地";
+	private static String firstName = "和樹";
 
-  public static void main(String[] args) {
-    Pet pet = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
-    RobotPet robotPet = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
-    printName();
-    pet.introduce();
-    robotPet.introduce();
-  }
+	public static void main(String[] args) {
+		Pet pet = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
+		RobotPet robotPet = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
+		printName(lastName, firstName);
+		pet.introduce();
+		robotPet.introduce();
+	}
 
-  private static void printName() {
-    System.out.println("printNameメソッド → " + firstName + lastName);
-  }
+	private static void printName(String ln, String fn) {
+		System.out.println("printNameメソッド → " + ln + fn);
+	}
 }
